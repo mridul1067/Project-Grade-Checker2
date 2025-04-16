300eb50fef13692ebfdc31818542601c79290789
mridul1067 <24bca20027@cuchd.in>
2025-04-16 13:51:02 +0530

    Update studentgradechecer.c

--- studentgradechecer.c
+++ studentgradechecer.c
@@ -21,10 +21,11 @@ int main() {
     int i;
 
     printf("Enter student name: ");
-    scanf(" %[^\n]", s.name);
+    scanf(" %[^\n]", s.name);  
     printf("Enter roll number: ");
     scanf("%d", &s.roll);
-    printf("Enter marks for 5 subjects:\n");
+    printf("Enter marks for 5 subjects:\n");  
+
     for (i = 0; i < 5; i++) {
         printf("Subject %d: ", i + 1);
         scanf("%f", &s.marks[i]);
@@ -34,10 +35,7 @@ int main() {
     percentage = total / 5;  
     char grade = calculateGrade(percentage);
 
-    printf("\n- Student Report -\n");
-    printf("Name: %s\n", s.name);
-    printf("Roll Number: %d\n", s.roll);
-    printf("Total Marks: %.2f\n", total);
+    printf("Marks: %.2f\n", total);
     printf("Percentage: %.2f%%\n", percentage);
     printf("Grade: %c\n", grade);
 
